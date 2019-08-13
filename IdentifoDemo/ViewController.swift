7666ef418e6b539431ffa497101d1b9aa344fe81
Dmytro Lisitsyn <dl@madappgang.com>
2019-08-13 17:46:34 +0300

    Initial Commit

---
+++ IdentifoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdentifoDemo
+//
+//  Created by Dmytro Lisitsyn on 8/13/19.
+//  Copyright © 2019 MadAppGang Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
