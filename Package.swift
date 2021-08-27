9019dde6605dc811c44f2e2af6a8e747803cc412
Nikolay Beznos <nb@madappgang.com>
2021-08-27 20:34:33 +0300

    spm test

--- Package.swift
+++ Package.swift
@@ -1,18 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Identifo",
-    platforms: [
-        .macOS(.v10_12), .iOS(.v11), .tvOS(.v11)
-    ],
+    name: "Identifo-ios",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Identifo",
-            targets: ["Identifo"]),
+            name: "Identifo-ios",
+            targets: ["Identifo-ios"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,12 +17,12 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Identifo",
+            name: "Identifo-ios",
             dependencies: []),
         .testTarget(
-            name: "IdentifoTests",
-            dependencies: ["Identifo"]),
+            name: "Identifo-iosTests",
+            dependencies: ["Identifo-ios"]),
     ]
 )
